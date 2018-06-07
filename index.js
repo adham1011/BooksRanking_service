6bb3de1a43de42efb452db90ffe8c8f8faca086a
Adham Khalilieh <Adham1011@github.com>
2018-06-07 10:01:18 +0300

    commit new

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const express    = require('express'),
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/Documentation', express.static('api'));
+app.use(
+ (req,res,next) => {
+ res.header("Access-Control-Allow-Origin", "*");
+ res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type,Accept");
+ next();
+});
 
 app.get('/',(req,res)=>{
   res.json({API:'https://books-ranking.herokuapp.com/Documentation/'});
